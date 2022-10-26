f33741edbb2c1544be6f4e57556aa025c392015f
Fazril235 <arielff54@gmail.com>
2022-10-26 21:18:10 +0700

    Single Post

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
-
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name"  => "Muhammad Fazril",
         "email" => "arielff54@gmail.com",
         "image" => "fazril.jpg"
@@ -27,6 +29,56 @@ Route::get('/about', function () {
 });
 
 
-Route::get('/', function () {
-    return view('post');
+
+Route::get('/blog', function () {
+    $blog_post = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Muhammad Fazril",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, et magnam laboriosam totam sapiente ipsam dolores saepe veniam cum vitae! Iste numquam vero odit totam quia eveniet sint, dicta nobis neque minus temporibus sunt natus obcaecati suscipit sed reiciendis hic illo ducimus corrupti harum delectus cum ad porro quis. Dolor quam illo nam ipsam alias laboriosam a eos accusamus, dolore unde dolores eveniet labore similique exercitationem quaerat sequi aliquam eum molestias nisi repellendus minima quo. Voluptatum sapiente aperiam eius. Minus."
+        ], 
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Alia Azzahra",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui dicta deleniti iure aliquid natus sapiente cupiditate laborum quas! Est eum magni similique nostrum aliquid doloribus aut rem? Non, quam praesentium maiores ea recusandae quia exercitationem assumenda eos necessitatibus molestiae dolore iusto quaerat, explicabo architecto incidunt! Quo expedita dolores tempore nesciunt aspernatur molestiae enim temporibus mollitia beatae, fuga, ratione ex architecto quae harum facere officia quam tempora optio, minus molestias. Cumque totam, quis non quod aperiam harum beatae natus dolores eius. Iste dolor harum quo? Doloremque fuga mollitia magnam atque dolor quaerat cum. Tenetur voluptate fugit at suscipit laboriosam explicabo magni."
+        ],
+    ];
+
+    return view('post', [
+        "title" => "Post",
+        "posts" => $blog_post
+    ]);
+});
+
+
+// halaman single post
+Route::get('post/{slug}', function($slug) {
+    $blog_post = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Muhammad Fazril",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus, et magnam laboriosam totam sapiente ipsam dolores saepe veniam cum vitae! Iste numquam vero odit totam quia eveniet sint, dicta nobis neque minus temporibus sunt natus obcaecati suscipit sed reiciendis hic illo ducimus corrupti harum delectus cum ad porro quis. Dolor quam illo nam ipsam alias laboriosam a eos accusamus, dolore unde dolores eveniet labore similique exercitationem quaerat sequi aliquam eum molestias nisi repellendus minima quo. Voluptatum sapiente aperiam eius. Minus."
+        ], 
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Alia Azzahra",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui dicta deleniti iure aliquid natus sapiente cupiditate laborum quas! Est eum magni similique nostrum aliquid doloribus aut rem? Non, quam praesentium maiores ea recusandae quia exercitationem assumenda eos necessitatibus molestiae dolore iusto quaerat, explicabo architecto incidunt! Quo expedita dolores tempore nesciunt aspernatur molestiae enim temporibus mollitia beatae, fuga, ratione ex architecto quae harum facere officia quam tempora optio, minus molestias. Cumque totam, quis non quod aperiam harum beatae natus dolores eius. Iste dolor harum quo? Doloremque fuga mollitia magnam atque dolor quaerat cum. Tenetur voluptate fugit at suscipit laboriosam explicabo magni."
+        ],
+    ];
+
+    $new_post = [];
+    foreach($blog_posts as $post) {
+        if($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+    return view('post', [ 
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
