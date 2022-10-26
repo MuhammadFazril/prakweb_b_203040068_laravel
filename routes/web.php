403d00b09f11388362a371958edc88d68558cdcc
Fazril235 <arielff54@gmail.com>
2022-10-26 19:51:52 +0700

    view

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+
+Route::get('/about', function () {
+    return view('about', [
+        "name"  => "Muhammad Fazril",
+        "email" => "arielff54@gmail.com",
+        "image" => "fazril.jpg"
+    ]);
+});
+
+
+Route::get('/', function () {
+    return view('post');
 });
