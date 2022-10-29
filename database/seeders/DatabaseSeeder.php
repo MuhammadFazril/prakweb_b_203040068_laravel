cd03d435e663e4e23c84d3c3cbf01a9cdad99489
Fazril235 <arielff54@gmail.com>
2022-10-29 20:36:38 +0700

    Factory & Faker

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,13 +2,11 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
-use App\Models\Category;
 use Illuminate\Database\Seeder;
-use\App\Models\User;
-use\App\Models\Category;
-use\App\Models\Post;
+use App\Models\User;
+use App\Models\Category;
+use App\Models\Post;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,70 +17,70 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
+        // User::create([
+        //     'name' => 'Muhammad Fazril',
+        //     'email' => '203040068@mail.unpas.ac.id',
+        //     'password' => bcrypt('12345')
         // ]);
 
-        User::create([
-            'name' => 'Muhammad Fazril',
-            'email' => '203040068@mail.unpas.ac.id',
-            'password' => bcrypt('12345')
-        ]);
+        // User::create([
+        //     'name' => 'Alia Azzahra',
+        //     'email' => 'hikfi71@gmail.com',
+        //     'password' => bcrypt('54321')
+        // ]);
 
-        User::create([
-            'name' => 'Alia Azzahra',
-            'email' => '203040068@mail.unpas.ac.id',
-            'password' => bcrypt('12345')
-        ]);
+
+        User::factory(3)->create();
 
         Category::create([
-            'name' => 'Web Programing',
-            'slug' => 'Web-programming'
+            'name' => 'Web Programming',
+            'slug' => 'web-programming'
         ]);
 
         Category::create([
             'name' => 'Personal',
-            'slug' => 'Personal'
+            'slug' => 'personal'
         ]);
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul pertama',
-            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus, maiores sunt expedita tempore perferendis quisquam cum quam odit. Adipisci cum vel eius excepturi illum non deserunt incidunt soluta quisquam suscipit?',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae eius aliquid quam unde omnis atque numquam ducimus distinctio totam facere? Eligendi id maiores cumque harum reiciendis ab vel est ea doloribus a. Vel eligendi sed iure laboriosam numquam laudantium vero sapiente excepturi doloremque eveniet! Assumenda eaque laborum cupiditate vitae amet aperiam mollitia aliquid debitis? Numquam ea dolor tenetur perspiciatis quas eveniet architecto fugit ducimus doloribus, ab, provident itaque quibusdam, adipisci et porro? Ut, fugiat. Dolorem eaque repudiandae atque voluptate reprehenderit.'
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        Post::factory(20)->create();
 
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa, fugiat labore aliquam esse odio deleniti!',
+        //     'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe veritatis animi facilis commodi facere nam quisquam nobis ab repudiandae</p>',
+        //     'category_id' => 1,
+        //     'user_id' => 1
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ke Dua',
-            'slug' => 'judul-ke-dua',
-            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus, maiores sunt expedita tempore perferendis quisquam cum quam odit. Adipisci cum vel eius excepturi illum non deserunt incidunt soluta quisquam suscipit?',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae eius aliquid quam unde omnis atque numquam ducimus distinctio totam facere? Eligendi id maiores cumque harum reiciendis ab vel est ea doloribus a. Vel eligendi sed iure laboriosam numquam laudantium vero sapiente excepturi doloremque eveniet! Assumenda eaque laborum cupiditate vitae amet aperiam mollitia aliquid debitis? Numquam ea dolor tenetur perspiciatis quas eveniet architecto fugit ducimus doloribus, ab, provident itaque quibusdam, adipisci et porro? Ut, fugiat. Dolorem eaque repudiandae atque voluptate reprehenderit.'
-            'category_id' => 1,
-            'user_id' => 1
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Dua',
+        //     'slug' => 'judul-ke-dua',
+        //     'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa, fugiat labore aliquam esse odio deleniti!',
+        //     'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe veritatis animi facilis commodi facere nam quisquam nobis ab repudiandae</p>',
+        //     'category_id' => 1,
+        //     'user_id' => 1
 
-        Post::create([
-            'title' => 'Judul Ke Tiga',
-            'slug' => 'Judul-ke-tiga',
-            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus, maiores sunt expedita tempore perferendis quisquam cum quam odit. Adipisci cum vel eius excepturi illum non deserunt incidunt soluta quisquam suscipit?',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae eius aliquid quam unde omnis atque numquam ducimus distinctio totam facere? Eligendi id maiores cumque harum reiciendis ab vel est ea doloribus a. Vel eligendi sed iure laboriosam numquam laudantium vero sapiente excepturi doloremque eveniet! Assumenda eaque laborum cupiditate vitae amet aperiam mollitia aliquid debitis? Numquam ea dolor tenetur perspiciatis quas eveniet architecto fugit ducimus doloribus, ab, provident itaque quibusdam, adipisci et porro? Ut, fugiat. Dolorem eaque repudiandae atque voluptate reprehenderit.'
-            'category_id' => 2,
-            'user_id' => 1
-        ]);
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ke Empat',
-            'slug' => 'Judul-ke-empat',
-            'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus, maiores sunt expedita tempore perferendis quisquam cum quam odit. Adipisci cum vel eius excepturi illum non deserunt incidunt soluta quisquam suscipit?',
-            'body' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Beatae eius aliquid quam unde omnis atque numquam ducimus distinctio totam facere? Eligendi id maiores cumque harum reiciendis ab vel est ea doloribus a. Vel eligendi sed iure laboriosam numquam laudantium vero sapiente excepturi doloremque eveniet! Assumenda eaque laborum cupiditate vitae amet aperiam mollitia aliquid debitis? Numquam ea dolor tenetur perspiciatis quas eveniet architecto fugit ducimus doloribus, ab, provident itaque quibusdam, adipisci et porro? Ut, fugiat. Dolorem eaque repudiandae atque voluptate reprehenderit.'
-            'category_id' => 2,
-            'user_id' => 2
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Tiga',
+        //     'slug' => 'judul-ke-tiga',
+        //     'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa, fugiat labore aliquam esse odio deleniti!',
+        //     'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe veritatis animi facilis commodi facere nam quisquam nobis ab repudiandae</p>',
+        //     'category_id' => 2,
+        //     'user_id' => 1
+
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Ke Empat',
+        //     'slug' => 'judul-ke-empat',
+        //     'excerpt' => 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Culpa, fugiat labore aliquam esse odio deleniti!',
+        //     'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Saepe veritatis animi facilis commodi facere nam quisquam nobis ab repudiandae</p>',
+        //     'category_id' => 2,
+        //     'user_id' => 2
+
+        // ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 use App\Models\Category;
+use App\Models\User;
 
 /*
 |--------------------------------------------------------------------------
@@ -53,3 +54,11 @@ Route::get('/categories/{category:slug}', function (Category $category) {
         'category' => $category->name,
     ]);
 });
+
+Route::get('/authors/{author:username}', function (User $author) {
+    return view('posts', [
+        'title' => User Posts,
+        'posts' => $author->posts,
+    ]);
+});
+
