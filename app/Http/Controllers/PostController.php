56f04e07f8c70a87d5211ff92db7332e936f57f7
Fazril235 <arielff54@gmail.com>
2022-11-04 10:27:38 +0700

    Menyelesaikan video 13 | Searching & Pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,33 +1,40 @@
-<?php 
+<?php
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Category;
 use App\Models\Post;
+use App\Models\User;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-  public function index()
-  {
-    $posts = Post::latest();
+    public function index()
+    {
+        $title = '';
 
-    if(request('search')) {
-      $posts->where('title', 'like', '%' . request('search') . '%');
+        if (request('category')) {
+            $category = Category::firstWhere('slug', request('category'));
+            $title = ' in ' . $category->name;
+        }
+
+        if (request('author')) {
+            $author = User::firstWhere('username', request('author'));
+            $title = ' by ' . $author->name;
+        }
+
+        return view('posts', [
+            "title" => "All Posts" . $title,
+            "active" => "posts",
+            "posts" => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(7)->withQueryString(),
+        ]);
     }
-
-    return view('posts', [
-      "title" => "All Posts",
-      "active" => 'posts',
-      "posts" => $posts->get()
-    ]);
-  }
-
-  public function show(Post $post)
-  {
-    return view('post', [ 
-      "title" => "Single Post",
-      "active" => 'posts',
-      "post" => $post
-  ]);
-  }
+    public function show(Post $post)
+    {
+        return view('post', [
+            "title" => "Single Post",
+            "active" => "posts",
+            "post" => $post,
+        ]);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,12 +9,34 @@ class Post extends Model
 {
     use HasFactory;
 
-    //protected $fillable = ['title', 'excerpt', 'body'];
+    // protected $fillable = ['title', 'excerpt', 'body'];
     protected $guarded = ['id'];
     protected $with = ['category', 'author'];
 
-    public function category()
-    {  
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, function($query, $search) {
+            return $query->where('tittle', 'like', '%' . $search . '%')
+                ->orWhere('body', 'like', '%' . $search . '%');
+
+        });
+
+        $query->when($filters['category'] ?? false, function($query, $category){
+            return $query->whereHas('category', function($query) use ($category) {
+                $query->where('slug', $category);
+            });
+        });
+
+        $query->when($filters['author'] ?? false, fn($query, $author) =>
+            $query->whereHas('author', fn($query) =>
+                $query->where('username', $author)
+            )
+        );
+
+    }
+
+    public function category() 
+    {
         return $this->belongsTo(Category::class);
     }
 
