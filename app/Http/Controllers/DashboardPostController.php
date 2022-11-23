7020c6f763e8547103dceebbd8fd43fe0d489bd5
Fazril235 <arielff54@gmail.com>
2022-11-23 12:08:48 +0700

    Menyelesaikan Video 19 | Validation & Insert Post

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -6,6 +6,7 @@ use App\Models\Post;
 use App\Models\Category;
 use Illuminate\Http\Request;
 use \Cviebrock\EloquentSluggable\Services\SlugService;
+use Illuminate\Support\Str;
 
 class DashboardPostController extends Controller
 {
@@ -16,7 +17,7 @@ class DashboardPostController extends Controller
      */
     public function index()
     {
-        return view('dashboard.post.index', [
+        return view('dashboard.posts.index', [
             'posts' => Post::where('user_id', auth()->user()->id)->get()
         ]);
     }
@@ -41,7 +42,18 @@ class DashboardPostController extends Controller
      */
     public function store(Request $request)
     {
-        return $request;
+        $validatedData = $request->validate([
+            'title' => 'required|max:255',
+            'slug' => 'required|unique:posts',
+            'category_id' => 'required',
+            'body' => 'required'
+        ]);
+
+        $validatedData['user_id'] = auth()->user()->id;
+        $validatedData['excerpt'] = Str::limit(strip_tags($request->body), 200);
+
+        Post::create($validatedData);
+        return redirect('/dashboard/posts')->with('success', 'New post has been added!');
     }
 
     /**
@@ -53,12 +65,12 @@ class DashboardPostController extends Controller
     public function show(Post $post)
     {
         return view('dashboard.posts.show', [
-            'posts' => $post
+            'post' => $post
         ]);
     }
 
     /**
-     * Sh ow the form for editing the specified resource.
+     * Show the form for editing the specified resource.
      *
      * @param  \App\Models\Post  $post
      * @return \Illuminate\Http\Response
